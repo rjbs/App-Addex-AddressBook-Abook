705bd541264afe86bc57cca6e99dec5759383856
Ricardo SIGNES <rjbs@cpan.org>
2009-07-08 13:17:55 -0400

    prep rerelease

--- lib/App/Addex/AddressBook/Abook.pm
+++ lib/App/Addex/AddressBook/Abook.pm
@@ -32,11 +32,11 @@ App::Addex::AddressBook::Abook - use the "abook" program as the addex source
 
 =head1 VERSION
 
-version 0.005
+version 0.006
 
 =cut
 
-our $VERSION = '0.005';
+our $VERSION = '0.006';
 
 =head1 SYNOPSIS
 
